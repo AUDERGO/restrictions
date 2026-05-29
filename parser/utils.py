77953c03279ea075865d914eae3b45d235e91856
AUDERGO <audrey.vernet@volvo.com>
2026-05-29 11:09:19 +0200

    Update utils.py

--- parser/utils.py
+++ parser/utils.py
@@ -1,1 +1,18 @@
+import re
 
+def normalize(text):
+    text = str(text).lower()
+
+    replacements = {
+        "é": "e", "è": "e", "ê": "e",
+        "à": "a", "ù": "u", "ô": "o"
+    }
+
+    for k, v in replacements.items():
+        text = text.replace(k, v)
+
+    return text
+
+
+def split_phrases(text):
+    return re.split(r"[.\n]", text)
